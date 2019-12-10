0da16b4cfabd8d4b7a1022699c853d92c554df3b
Hartjes <DHartjes@OldRepublicTitle.com>
2019-12-10 08:38:56 -0600

    Day 2.1 progress

--- .compiled/Day2/paperProvider.test.js
+++ .compiled/Day2/paperProvider.test.js
@@ -14,10 +14,14 @@ describe("Determines how many sq feet of wrapping paper are needed for an array 
   it("should be 20 for a 2x2x1 cube", () => {
     expect((0, _paperProvider.default)("2x2x1")).toBe(20);
   });
-  it("should be 20 for a 2x2x1 cube", () => {
+  it("should be 40 for cubes [2x1x1,1x1x1,2x2x1]", () => {
     let input = `2x1x1
 		1x1x1
 		2x2x1`;
+    expect((0, _paperProvider.default)(input)).toBe(40);
+  });
+  it("should be big for a 111x222x333 cube", () => {
+    let input = "1111x222x3333";
     expect((0, _paperProvider.default)(input)).toBe(40);
   });
 });

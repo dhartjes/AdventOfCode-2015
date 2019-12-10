627371bb18e74724734fe9c65d36d5461143e55e
dhartjes <dhartjes@gmail.com>
2019-12-10 11:12:49 -0600

    Day 2.1 complete

--- src/Day2/paperProvider.js
+++ src/Day2/paperProvider.js
@@ -1,9 +1,9 @@
 const measurePaper = box => {
   let dimensions = box.split("x");
-  let h = 2 * Number.parseInt(dimensions[0]) * Number.parseInt(dimensions[1]);
-  let w = 2 * Number.parseInt(dimensions[1]) * Number.parseInt(dimensions[2]);
-  let d = 2 * Number.parseInt(dimensions[0]) * Number.parseInt(dimensions[2]);
-  let surfaceArea = h + w + d;
+  let h = Number.parseInt(dimensions[0]) * Number.parseInt(dimensions[1]);
+  let w = Number.parseInt(dimensions[1]) * Number.parseInt(dimensions[2]);
+  let d = Number.parseInt(dimensions[0]) * Number.parseInt(dimensions[2]);
+  let surfaceArea = 2 * h + 2 * w + 2 * d;
   let slack = Math.min(h, w, d);
   return surfaceArea + slack;
 };
@@ -12,8 +12,9 @@ const paperProvider = boxDimensionsList => {
   let boxDimensionsArray = boxDimensionsList
     .replace(/\r\n/g, "\n")
     .replace(/\r/g, "\n")
-    .replace(/\n/g, "|")
-    .split("|");
+    .trim()
+    .split("\n")
+    .filter(Boolean);
 
   var sum = boxDimensionsArray.reduce(
     (accumulator, current) => accumulator + measurePaper(current),
--- src/Day2/paperProvider.test.js
+++ src/Day2/paperProvider.test.js
@@ -1,27 +1,48 @@
 import paperProvider from "./paperProvider";
+import getAocInput from "../utils/aocInputFetcher";
 
 describe("Determines how many sq feet of wrapping paper are needed for an array of boxes", () => {
   it("should be 8 for 1x1x1 cube", () => {
-    expect(paperProvider("1x1x1")).toBe(8);
+    expect(paperProvider("1x1x1")).toBe(7);
   });
 
   it("should be 12 for a 2x1x1 cube", () => {
-    expect(paperProvider("2x1x1")).toBe(12);
+    expect(paperProvider("2x1x1")).toBe(11);
   });
 
   it("should be 20 for a 2x2x1 cube", () => {
-    expect(paperProvider("2x2x1")).toBe(20);
+    expect(paperProvider("2x2x1")).toBe(18);
   });
 
   it("should be 40 for cubes [2x1x1,1x1x1,2x2x1]", () => {
     let input = `2x1x1
 		1x1x1
-		2x2x1`;
-    expect(paperProvider(input)).toBe(40);
+    2x2x1`;
+    expect(paperProvider(input)).toBe(36);
+  });
+
+  it("should handle inputs with empty lines", () => {
+    let input = `2x1x1
+    `;
+    expect(paperProvider(input)).toBe(11);
+  });
+
+  it("should be a number using values from AOC", async () => {
+    const aocInput = await getAocInput(
+      "https://adventofcode.com/2015/day/2/input"
+    );
+
+    expect(aocInput).not.toBeNull();
+    aocInput.split("\n");
+    var result = paperProvider(aocInput);
+
+    console.log(result);
+    expect(result).not.toBeNaN();
+    expect(result).toBe(1588178);
   });
 
   it("should be big for a 111x222x333 cube", () => {
     let input = "1111x222x3333";
-    expect(paperProvider(input)).toBe(40);
+    expect(paperProvider(input)).toBe(9625704);
   });
 });

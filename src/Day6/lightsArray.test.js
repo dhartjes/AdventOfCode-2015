ad7efb29d6babef740217049e3e65d2bd3681fb9
Hartjes <DHartjes@OldRepublicTitle.com>
2019-12-23 09:53:06 -0600

    Day 6 new files

---
+++ src/Day6/lightsArray.test.js
@@ -0,0 +1,8 @@
+describe("lightsArray maintains state of the lights between instructions", () => {
+  it("should instantiate lights array of specified size", () => {
+    let lightsArray = new lightsArray(3, 3);
+  });
+  it("should return total number of lights lit", () => {});
+
+  it("should maintain state of the light array", () => {});
+});
---
+++ src/Day6/lightsParser.test.js
@@ -0,0 +1,11 @@
+import sut from "./lightsParser";
+
+describe("lightsParser takes input and converts instructions to perform on lights array", () => {
+  it("should read turn on as turnOn function", () => {});
+
+  it("should read turn off as turnOff function", () => {});
+
+  it("should read toggle as toggle function", () => {});
+
+  it("should read x1,y1 through x2,y2 as referring to specific lights in the array", () => {});
+});
